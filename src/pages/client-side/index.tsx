11a15dba4fbc2bcffa84cb997544858c05e823ed
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 22:51:58 +0100

    feat(client-side): Add small description text

--- src/pages/client-side/index.tsx
+++ src/pages/client-side/index.tsx
@@ -1,8 +1,10 @@
 
 const HarryPotter = () => {
   return (
-    <div>
-      Harry Potter Pages using Client-Side rendering. Explore the navigation tabs to access the DB.
+    <div className="w-8/12 mx-auto mt-4">
+      <p>Harry Potter Pages using Client-Side rendering. Explore the navigation tabs to access the pages.</p>
+      <p>Check the Network tab to see how the files are sent to the browser to render the pages.</p>
+      <p>A more detailed explaination of the Client-Side rendering process will come soon to this page :)</p>
     </div>
   )
 }
