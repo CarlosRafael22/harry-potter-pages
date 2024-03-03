693205c2d9a3fc90eb050cedb2fca3edcafd4097
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 22:51:40 +0100

    feat(client-side/books): Flex wrap items

--- src/pages/client-side/books/index.tsx
+++ src/pages/client-side/books/index.tsx
@@ -16,10 +16,12 @@ const Books = () => {
 
   return (
     <div>
-      Harry Potter books:
-      {books?.map((book) => {
+      <p className='subtitle-1 text-center'>Harry Potter books:</p>
+      <div className='flex flex-wrap'>
+        {books?.map((book) => {
           return <BookCard {...book.attributes} key={book.id} />
         })}
+      </div>
     </div>
   )
 }
