fb064eb2e69d54241bbf9f2a14745530d849a8b7
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 09:50:51 +0100

    feat(next.config.mjs): Allow images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+        port: '',
+        pathname: '/harrypotter/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
