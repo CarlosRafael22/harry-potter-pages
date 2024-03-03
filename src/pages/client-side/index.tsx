7fc6521b00e773dde599bbb4bc2a6aae57db0d6c
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 09:48:04 +0100

    feat(client-side): Create index file

---
+++ src/pages/client-side/index.tsx
@@ -0,0 +1,10 @@
+
+const HarryPotter = () => {
+  return (
+    <div>
+      Harry Potter Pages using Client-Side rendering. Explore the navigation tabs to access the DB.
+    </div>
+  )
+}
+
+export default HarryPotter
