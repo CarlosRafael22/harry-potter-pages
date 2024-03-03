3ac4386e59115243efc2f05cd524088cc5460748
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 09:52:05 +0100

    feat(client-side/characters): Create index route

---
+++ src/pages/client-side/characters/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { getCharactersData } from '@/services/characters'
+import CharacterCard from '@/components/molecules/characters/CharacterCard'
+
+const Characters = () => {
+    const [characters, setCharacters] = useState<any[]>()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getCharactersData()
+            setCharacters(result.data)
+        }
+        fetchData()
+    }, [])
+
+  return (
+    <div>
+      <p>Harry Potter characters:</p>
+      <div className='flex flex-row flex-wrap gap-4 w-10/12 mx-auto justify-between'>
+        {characters?.map((book) => {
+            return <CharacterCard {...book.attributes} id={book.id} key={book.id} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Characters
