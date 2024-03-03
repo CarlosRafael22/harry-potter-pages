27e18dd7554af5a2b9b55abfcc43cf347a365752
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 18:57:08 +0100

    feat(Footer): Small style changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
     return (
-        <div className="border-2 border-solid border-red-400 p-4 w-full">
-            <p>Harry Potter Pages - Using https://potterdb.com/</p>
+        <div className="border-t-2 border-solid border-white p-4 w-full">
+            <p className="subtitle-1">Harry Potter Pages - Using https://potterdb.com/</p>
         </div>
     )
 }
