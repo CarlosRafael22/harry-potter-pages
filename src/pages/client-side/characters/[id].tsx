28e6b76964dc2b1ba46e5e47bb71ae4b130cbf98
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 09:52:28 +0100

    feat(client-side/characters): Create dynamic route

---
+++ src/pages/client-side/characters/[id].tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+import { getCharacterDataById } from '@/services/characters'
+import Character from '@/components/organisms/characters/Character'
+import { CharacterAttributes } from '@/entities/characters'
+import { ResponseData } from '@/entities/types'
+
+const Characters = () => {
+    const { query, isReady } = useRouter()
+    const [character, setCharacter] = useState<ResponseData<CharacterAttributes> | undefined>()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getCharacterDataById(query.id as string)
+            setCharacter(result.data)
+            setLoading(false)
+        }
+        if (isReady && query.id) {
+            fetchData()
+        }
+    }, [isReady, query.id])
+
+  return (
+    <div>
+      <p>Harry Potter character:</p>
+      {
+        loading ? <p>Loading....</p> : (
+            <div className='flex flex-row flex-wrap gap-4 w-10/12 mx-auto justify-between'>
+                <Character {...character!.attributes} />
+            </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default Characters
