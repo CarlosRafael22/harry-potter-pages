87cb165b8d583adb65736fbb89741deb691fed4d
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-03 22:50:07 +0100

    feat(BookCard): Add image and SimpleInfoTable

--- src/components/molecules/books/BookCard.tsx
+++ src/components/molecules/books/BookCard.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image'
+
 import { BookAttributes } from '@/entities/books'
+import SimpleInfoTable from '../SimpleInfoTable'
 
 const BookCard = (props: BookAttributes) => {
   return (
-    <div className="border-2 border-solid border-red-400 p-4">
-        <p>{`Title: ${props.title}`}</p>
-        <p>{`Pages: ${props.pages}`}</p>
-        <p>{`Dedication: ${props.dedication}`}</p>
-        <p>{`Author: ${props.author}`}</p>
+    <div className="flex flex-col gap-2 p-4 w-1/4">
+        {props.cover && <Image
+                src={props.cover}
+                width={200}
+                height={200}
+                alt={props.title || 'Book cover'}
+            />}
+        <SimpleInfoTable>
+            <SimpleInfoTable.Row title='Title' value={props.title} />
+            <SimpleInfoTable.Row title='Pages' value={`${props.pages}`} />
+            <SimpleInfoTable.Row title='Dedication' value={props.dedication} />
+            <SimpleInfoTable.Row title='Author' value={props.author} />
+        </SimpleInfoTable>
     </div>
   )
 }
