35123a9e076e4759627b4aefc5d9f415b0a1c8bc
Rafael Leitao <carlosrafael093@gmail.com>
2024-03-10 21:40:16 +0100

    feat(services/characters): Improve types

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -1,9 +1,11 @@
 import { makeRESTRequestToEndpoint } from "@/helpers/requests"
+import { Response } from '@/entities/types'
+import { CharacterAttributes } from '@/entities/characters'
 
-export const getCharactersData = async () => {
+export const getCharactersData = async (): Promise<Response<CharacterAttributes[]>> => {
   return await makeRESTRequestToEndpoint('/characters')
 }
 
-export const getCharacterDataById = async (id: string) => {
+export const getCharacterDataById = async (id: string): Promise<Response<CharacterAttributes>> => {
   return await makeRESTRequestToEndpoint(`/characters/${id}`)
 }
